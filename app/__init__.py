1d954a805c4df893184b8d2c697b1333bb4f4399
aviiii <avi.j@somaiya.edu>
2021-08-07 11:07:48 -0400

    Basic Architechture and Flow

---
+++ app/__init__.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template('index.html')
+
+@app.route("/shop")
+def shop():
+    return render_template('shop.html')
+
+@app.route("/cart")
+def cart():
+    return render_template('cart.html')
+
+@app.route("/confirm")
+def confirm():
+    return render_template('confirm.html')
