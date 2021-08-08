1aaf805b77b868a8b695f83c5ee840e4f71e7e38
Lint Action <lint-action@samuelmeuli.com>
2021-08-08 01:01:09 +0000

    Fix code style issues with Black

--- app/__init__.py
+++ app/__init__.py
@@ -2,18 +2,22 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def home():
-    return render_template('index.html')
+    return render_template("index.html")
+
 
 @app.route("/shop")
 def shop():
-    return render_template('shop.html')
+    return render_template("shop.html")
+
 
 @app.route("/cart")
 def cart():
-    return render_template('cart.html')
+    return render_template("cart.html")
+
 
 @app.route("/confirm")
 def confirm():
-    return render_template('confirm.html')
+    return render_template("confirm.html")
