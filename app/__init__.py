2c3fffd54611d298f32047b30e44c02e8988f4d2
Marilyn Balkaransingh <marilynbalkaransingh@marilyns-air.home>
2021-08-10 18:47:27 -0400

    add login and register routes based on course material

--- app/__init__.py
+++ app/__init__.py
@@ -184,3 +184,62 @@ def confirm():
 @app.route("/health")
 def health():
     return "Hello, This is a Health Check and also a workflow check"
+
+# @app.route("/login", methods=("GET", "POST"))
+# def login():
+#     if request.method == "POST":
+#         username = request.form.get("username")
+#         password = request.form.get("password")
+#         error = None
+#         user = UserModel.query.filter_by(username=username).first()
+
+#         if user is None:
+#             error = "Incorrect username."
+#         elif not check_password_hash(user.password, password):
+#             error = "Incorrect password."
+
+#         if error is None:
+#             return "Login Successful", 200
+#         else:
+#             return error, 418
+
+#     ## TODO: Return a login page
+#     # return "Login page not yet implemented", 501
+#     return (
+#         render_template(
+#             "login.html", title="Brandon Balkaransingh", url=os.getenv("URL")
+#         ),
+#         200,
+#     )
+
+
+# @app.route("/register", methods=("GET", "POST"))
+# def register():
+#     if request.method == "POST":
+#         username = request.form.get("username")
+#         password = request.form.get("password")
+#         error = None
+
+#         if not username:
+#             error = "Username is required."
+#         elif not password:
+#             error = "Password is required."
+#         elif UserModel.query.filter_by(username=username).first() is not None:
+#             error = f"User {username} is already registered."
+
+#         if error is None:
+#             new_user = UserModel(username, generate_password_hash(password))
+#             db.session.add(new_user)
+#             db.session.commit()
+#             return f"User {username} created successfully"
+#         else:
+#             return error, 418
+
+#     ## TODO: Return a restister page
+#     # return "Register Page not yet implemented", 501
+#     return (
+#         render_template(
+#             "register.html", title="Brandon Balkaransingh", url=os.getenv("URL")
+#         ),
+#         200,
+#     )
