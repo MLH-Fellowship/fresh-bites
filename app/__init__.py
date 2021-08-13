af0cea9cbbafe0d851938145224a1d054bfa4bb6
Lint Action <lint-action@samuelmeuli.com>
2021-08-13 04:42:35 +0000

    Fix code style issues with Black

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,14 @@
 import os
-from flask import Flask, render_template, send_from_directory, request, redirect, jsonify, session, url_for
+from flask import (
+    Flask,
+    render_template,
+    send_from_directory,
+    request,
+    redirect,
+    jsonify,
+    session,
+    url_for,
+)
 from dotenv import load_dotenv, find_dotenv
 from werkzeug.security import generate_password_hash, check_password_hash
 from werkzeug.exceptions import HTTPException
@@ -17,72 +26,78 @@ app = Flask(__name__)
 oauth = OAuth(app)
 
 app.config["DATABASE"] = os.path.join(os.getcwd(), "flask.frm")
-app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 
 auth0 = oauth.register(
-    'auth0',
+    "auth0",
     client_id=os.getenv("CLIENT_ID"),
     client_secret=os.getenv("CLIENT_SECRET"),
-    api_base_url='https://aviiii.us.auth0.com',
-    access_token_url='https://aviiii.us.auth0.com/oauth/token',
-    authorize_url='https://aviiii.us.auth0.com/authorize',
+    api_base_url="https://aviiii.us.auth0.com",
+    access_token_url="https://aviiii.us.auth0.com/oauth/token",
+    authorize_url="https://aviiii.us.auth0.com/authorize",
     client_kwargs={
-        'scope': 'openid profile email',
+        "scope": "openid profile email",
     },
 )
 
 
-@app.route('/callback')
+@app.route("/callback")
 def callback_handling():
     auth0.authorize_access_token()
-    resp = auth0.get('userinfo')
+    resp = auth0.get("userinfo")
     userinfo = resp.json()
     print(userinfo)
-    session['jwt_payload'] = userinfo
-    session['profile'] = {
-        'user_id': userinfo['sub'],
-        'name': userinfo['name'],
-        'picture': userinfo['picture'],
-        'username': userinfo['nickname'],
+    session["jwt_payload"] = userinfo
+    session["profile"] = {
+        "user_id": userinfo["sub"],
+        "name": userinfo["name"],
+        "picture": userinfo["picture"],
+        "username": userinfo["nickname"],
     }
-    return redirect('/')
+    return redirect("/")
 
 
-@app.route('/login')
+@app.route("/login")
 def login():
-    return auth0.authorize_redirect(redirect_uri='https://fresh-bites.tech/callback')
+    return auth0.authorize_redirect(redirect_uri="https://fresh-bites.tech/callback")
 
 
 def requires_auth(f):
     @wraps(f)
     def decorated(*args, **kwargs):
-        if 'profile' not in session:
+        if "profile" not in session:
             # Redirect to Login page here
-            return redirect('/login')
+            return redirect("/login")
         return f(*args, **kwargs)
 
     return decorated
+
 
 # @app.route("/")
 # def home():
 #     return render_template("index.html")
 
 
-@app.route('/')
+@app.route("/")
 def dashboard():
-    print(session.get('profile')    )
-    return render_template('index.html',
-                           userinfo=session.get('profile'),)
+    print(session.get("profile"))
+    return render_template(
+        "index.html",
+        userinfo=session.get("profile"),
+    )
 
 
-@app.route('/logout')
+@app.route("/logout")
 @requires_auth
 def logout():
     # Clear session stored data
     session.clear()
     # Redirect user to logout endpoint
-    params = {'returnTo': 'https://fresh-bites.tech/', 'client_id': os.getenv('CLIENT_ID')}
-    return redirect(auth0.api_base_url + '/v2/logout?' + urlencode(params))
+    params = {
+        "returnTo": "https://fresh-bites.tech/",
+        "client_id": os.getenv("CLIENT_ID"),
+    }
+    return redirect(auth0.api_base_url + "/v2/logout?" + urlencode(params))
 
 
 @app.route("/shop")
